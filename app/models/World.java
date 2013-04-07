9e747eb2f63c4871b0c39944713cbf1284ed904b
Tom Carchrae <tom@intellecti.ca>
2013-04-07 04:32:51 -0700

    set the id

--- app/models/World.java
+++ app/models/World.java
@@ -4,14 +4,25 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import play.db.jpa.GenericModel;
 import play.db.jpa.Model;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * use a generic model as we want to explicitly define the id
+ * 
+ * @author tom
+ * 
+ */
 @Entity
-public class World extends Model {
+public class World extends GenericModel {
 
-    @Column(name = "randomNumber")
-    public Long randomNumber;
+	public World(long i) {
+		id = i;
+	}
+
+	@Id
+	public Long id;
 
 }
